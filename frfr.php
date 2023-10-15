6ad99e2ff3792f233bb8ef41cabbc23ed733d966
eloo555 <planetagieror@gmail.com>
2023-10-15 18:43:06 +0200

    up

--- frfr.php
+++ frfr.php
@@ -1,14 +1,9 @@
 <?php
-@$a = $_POST['a'];
-@$b = $_POST['b'];
-@$opcja = $_POST['opcja'];
+$opcja = $_POST['opcja'];
 
-if($opcja == "a")
-{
-    echo "Niestety, ale masz rację";
-}
-else if($opcja == "b")
-{
+if($opcja == "a") {
+    echo "Niestety, masz rację";
+} else if($opcja == "b") {
     echo "Nie masz racji";
 }
 ?>
